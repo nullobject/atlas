e0c59ed0df315484c710224e2623deff356af34c
Josh Bassett <josh.bassett@gmail.com>
2012-07-08 14:29:59 +0200

    Use case classes.

--- hello.scala
+++ hello.scala
@@ -1,11 +1,14 @@
-/* import com.fasterxml.jackson.annotation.JsonProperty; */
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.core.`type`.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
 import java.lang.reflect.{Type, ParameterizedType}
 
-case class Gene(name: String, value: Long)
+@JsonTypeInfo(use=JsonTypeInfo.Id.MINIMAL_CLASS, include=JsonTypeInfo.As.PROPERTY, property="name")
+abstract class Gene(value: Long)
+case class FeedFrequency(value: Long) extends Gene(value)
+case class ReproduceFrequency(value: Long, minParents: Long) extends Gene(value)
 
 object Hello {
   val mapper = new ObjectMapper()
@@ -20,8 +23,8 @@ object Hello {
       }
     })
 
-  val json = """{"name":"FeedFrequency","value":1}"""
-  val node = deserialize[Gene](json)
+  val json = """[{"name":"FeedFrequency","value":1},{"name":"ReproduceFrequency","value":2,"minParents":2}]"""
+  val result = deserialize[Seq[Gene]](json)
 
-  def main(args: Array[String]) = println(node)
+  def main(args: Array[String]) = println(result)
 }
