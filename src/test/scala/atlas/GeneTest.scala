828112816060b131627cf8e38c3497ecaf0bcb4a
Josh Bassett <josh.bassett@gmail.com>
2012-11-29 19:35:17 +1100

    Added Gene.correlate method.

--- src/test/scala/atlas/GeneTest.scala
+++ src/test/scala/atlas/GeneTest.scala
@@ -11,4 +11,28 @@ class GeneTest extends FunSpec {
       assert(result.value === 0.25)
     }
   }
+
+  describe(".correlation") {
+    it("should calculate the Pearson product-moment correlation coefficient") {
+      val xs = Seq(1.0, 2.0, 3.0)
+      val ys = Seq(1.0, 3.0, 2.0)
+      assert(Gene.correlation(xs, ys) === 0.5)
+    }
+
+    it("should throw an error when the sequences have different sizes") {
+      intercept[IllegalArgumentException] {
+        val xs = Seq(1.0, 2.0, 3.0)
+        val ys = Seq(1.0, 3.0)
+        Gene.correlation(xs, ys)
+      }
+    }
+
+    it("should throw an error when the sequences are empty") {
+      intercept[IllegalArgumentException] {
+        val xs = Seq.empty
+        val ys = Seq.empty
+        Gene.correlation(xs, ys)
+      }
+    }
+  }
 }
