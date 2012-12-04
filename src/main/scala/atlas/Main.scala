c62b9bf83dbe58c41970b973e45758d9abf4a327
Josh Bassett <josh.bassett@gmail.com>
2012-12-04 22:12:06 +1100

    Refactored main.

--- src/main/scala/atlas/Main.scala
+++ src/main/scala/atlas/Main.scala
@@ -6,15 +6,9 @@ object Main {
   val system = ActorSystem()
 
   def main(args: Array[String]) {
-    val organism1 = Organism()
-
-    val cell1 = Cell(position = (0, 0), organisms = Set(organism1))
-    val cell2 = Cell(position = (1, 0))
-    val cell3 = Cell(position = (0, 1))
-    val cell4 = Cell(position = (1, 1))
-
-    val world = World(cells = Set(cell1, cell2, cell3, cell4))
-
+    val size = 4
+    val cells = for (x <- 0 until size; y <- 0 until size) yield Cell(position = (x, y))
+    val world = World(cells = cells.toSet)
     var game = system.actorOf(Props(new Game(world)), name = "game")
     var server = system.actorOf(Props(new Server(game)), name = "server")
   }
